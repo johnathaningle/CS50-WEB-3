3028198dbc37e8e24a1e459e9e75ec29f3929429
Johnathan Ingle <jingle1000@gmail.com>
2019-01-04 12:33:29 -0500

    Working Login/Register Redirect

--- application.py
+++ application.py
@@ -1,24 +1,31 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash, url_for
 from forms import RegistrationForm, LoginForm
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'efs2@$$2f0-pl,<'
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Book Review Network Home')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@email.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', form=form)
 
 if __name__ == "__main__":
