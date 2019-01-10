7c20688802a66284ce44458ff2338e3f1050f0da
Johnathan Ingle <jingle1000@gmail.com>
2019-01-10 11:16:58 -0500

    Add db migration features
    flask_migrate - alembic fork

--- booknetwork/__init__.py
+++ booknetwork/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
@@ -8,10 +9,13 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'efs2@$$2f0-pl,<'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db =SQLAlchemy(app)
 
 loginmanager = LoginManager(app)
 loginmanager.login_view = 'login'
 
-from booknetwork import routes
+migrate = Migrate(app, db)
+
+from booknetwork import routes, models
