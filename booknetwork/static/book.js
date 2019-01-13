3fc738765a2935f5d0edbcca07376f0b4c210214
Johnathan Ingle <jingle1000@gmail.com>
2019-01-13 15:34:08 -0500

    Add Goodreads API Feature

--- booknetwork/static/book.js
+++ booknetwork/static/book.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(e){
-    
+$(document).ready(function(){
+    let rating = $("#rating-number").text();
+    console.log(rating);
+    var options = {
+        max_value: 5,
+        step_size: 0.5,
+        selected_symbol_type: 'utf8_star',
+        initial_value: rating,
+        update_input_field_name: $("#rater"),
+    }
+    $("#rate").rate(options);
+
 });
