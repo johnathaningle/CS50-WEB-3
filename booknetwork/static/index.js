fc106471351718b689d41c476c15b48b73d21e0f
Johnathan Ingle <jingle1000@gmail.com>
2019-01-13 11:22:04 -0500

    Working Search Page
    divs appended to search results after querying database

--- booknetwork/static/index.js
+++ booknetwork/static/index.js
@@ -1,7 +1,8 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function(e){
+    var searchContentDiv = document.getElementById('search-results');
     document.getElementById('search-button').addEventListener('click', function(e) {
         e.preventDefault();
-        document.getElementById('search-results').style.display = "block";
+        searchContentDiv.style.display = "block";
         var searchValue = document.getElementById('search').value;
         console.log(searchValue);
         load_page(searchValue);
@@ -14,12 +15,20 @@ window.onload = function () {
         request.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 data = this.responseText;
+                data = JSON.parse(data);
                 console.log(data);
-                return data;
+                console.log(data["data"][0].author);
+                console.log(data["data"].length);
+                for (i = 0; i < data["data"].length; i++) {
+                    let searchDiv = document.createElement('div');
+                    searchDiv.className = "row content-section content-section-result pt-4 mt-4";
+                    searchDiv.innerHTML = `<div class='col-md-3'><p><a href='#'>${data["data"][i].isbn}</a></p></div><div class='col-md-3'>${data["data"][i].author}</div><div class='col-md-3'>${data["data"][i].title}</div><div class='col-md-3'>${data["data"][i].year_published}</p></div>`;
+                    searchContentDiv.appendChild(searchDiv);
+                }
             } 
         };
         request.open('GET', `/search/${text}`, true);
-        request.send();
-        
+        request.send(); 
     }
-};
+});
+   
